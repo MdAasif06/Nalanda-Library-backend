4a00176a3971272ba5eb0814b0e2846f69ff7ded
MdAasif06 <abdurm477@gmail.com>
2026-01-10 23:21:47 +0530

    database connected successfully

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import {connectDB} from "./configs/Db.js";
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database call
+connectDB();
+
+// Routes
+// app.use("/api/auth", require("./routes/auth.routes"));
+// app.use("/api/books", require("./routes/book.routes"));
+// app.use("/api/borrow", require("./routes/borrow.routes"));
+// app.use("/api/reports", require("./routes/report.routes"));
+
+//port define
+const PORT = process.env.PORT || 5000;
+
+//server is running
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
